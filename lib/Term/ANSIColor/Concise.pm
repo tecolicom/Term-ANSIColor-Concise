dee97c835fee98d9f14a6098c87e61fdf0992174
Kazumasa Utashiro <kaz@utashiro.com>
2023-04-17 13:10:20 +0900

    do not produce EL after newline

--- lib/Term/ANSIColor/Concise.pm
+++ lib/Term/ANSIColor/Concise.pm
@@ -362,7 +362,7 @@ sub apply_color {
 	my($s, $e, $el) = @{ $cache->{$color} //= [ ansi_pair($color) ] };
 	state $reset = qr{ \e\[[0;]*m (?: \e\[[0;]*[Km] )* }x;
 	if ($el) {
-	    $text =~ s/(\A|(?<=[\r\n])|$reset)([^\e\r\n]*)/${1}${s}${2}${e}/mg;
+	    $text =~ s/(\A|(?<=[\r\n])|$reset)([^\e\r\n]+|(?<!\n))/${1}${s}${2}${e}/mg;
 	} else {
 	    $text =~ s/(\A|(?<=[\r\n])|$reset)([^\e\r\n]+)/${1}${s}${2}${e}/mg;
 	}
